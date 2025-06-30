5bcd00fc13e432a10f1d0acc87c21faa05e9d3fe
Your Name <your-email@example.com>
2025-06-30 14:30:08 +0700

    update

---
+++ include/Header_EDU/Login_Form.h
@@ -0,0 +1,15 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include "../Header_School/HinsyOOP"
+
+using namespace HinsyOOP;
+
+void test() {
+    string num;
+    
+    cout << "Enter Number of NUm";HLVInput::inputNumber(num);
+   
+}
+
+#endif // TEST_HPP
